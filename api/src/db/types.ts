32f702e56111d3c063e7532cbbb63aa1c3a27d3f
Israel Dryer <israel.dryer@gmail.com>
2026-09-05 18:36:31 -0400

    clear remnants of drizzle

--- api/src/db/types.ts
+++
@@ -1,7 +0,0 @@
-import {customType} from "drizzle-orm/pg-core";
-
-export const tstzrange = customType<{ data: string, driverData: string }>({
-    dataType() {
-        return 'tstzrange';
-    }
-});
